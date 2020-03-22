5289a0a4ea3979e1afb8252ef2b9c93d35dde467
Lucio Ticali <info@freewebsolution.it>
2020-03-22 11:24:45 +0100

    Istallazione json-server, bootstrap, font-awesome, preparazione componenti e clean-blog

--- book/src/app/app.module.ts
+++ book/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BookComponent } from './components/book/book.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
